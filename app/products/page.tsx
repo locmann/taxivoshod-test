86cceb846e898a82e915b773de41ce6bb9e48d59
locmann <b0ne7228@gmail.com>
2024-06-18 23:41:42 +0300

    feat: wrap suspense

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,16 @@
 import Pagination from '@/components/Pagination/Pagination';
 import Products from '@/components/Products/Products';
 import Filter from '@/components/Filter/Filter';
+import { Suspense } from 'react';
 
 const ProductsPage = () => {
   return (
     <div>
       <Filter />
       <Products />
-      <Pagination />
+      <Suspense>
+        <Pagination />
+      </Suspense>
     </div>
   );
 };
